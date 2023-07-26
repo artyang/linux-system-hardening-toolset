9d4628d4697e3a24c4a8c1c161dba0a0dc614022
AlphaKiloNine <33735895+artyang@users.noreply.github.com>
2023-07-25 22:36:20 -0700

    Create install_bsdgames.sh

---
+++ install_bsdgames.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Function to install bsdgames
+install_bsdgames() {
+    echo "Installing bsdgames..."
+    sudo apt-get update
+    sudo apt-get install -y bsdgames
+
+    echo "bsdgames installed successfully!"
+}
+
+# Check if the script is run with sudo/root privileges
+if [ "$(id -u)" != "0" ]; then
+    echo "This script needs to be run with sudo or as the root user."
+    exit 1
+fi
+
+# Call the function to install bsdgames
+install_bsdgames
