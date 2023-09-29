dbe6b0a72d1702b93c5b5b32ffba4edcbd20f1ad
AlphaKiloNine <33735895+artyang@users.noreply.github.com>
2023-09-29 11:30:10 -0700

    Update setup-openssl.sh

--- setup-openssl.sh
+++ setup-openssl.sh
@@ -23,21 +23,21 @@ echo "Self-signed certificate generated:"
 openssl x509 -in /etc/ssl/certs/server.crt -noout -text
 
 echo "OpenSSL setup completed."
-This script performs the following actions:
+#This script performs the following actions:
 
-Checks if the script is being run as the root user.
-Installs OpenSSL using apt-get.
-Generates a self-signed certificate using openssl req.
-Sets permissions for the private key file.
-Displays the generated certificate information using openssl x509.
-To use the script, save it to a file (e.g., setup-openssl.sh), make it executable (chmod +x setup-openssl.sh), and run it with root privileges:
+#Checks if the script is being run as the root user.
+#Installs OpenSSL using apt-get.
+#Generates a self-signed certificate using openssl req.
+#Sets permissions for the private key file.
+#Displays the generated certificate information using openssl x509.
+#To use the script, save it to a file (e.g., setup-openssl.sh), make it executable (chmod +x setup-openssl.sh), and run it with root privileges:
 
-bash
-Copy code
-sudo ./setup-openssl.sh
-The script will install OpenSSL, generate a self-signed certificate, set permissions for the private key, and display the generated certificate information.
+#bash
+#Copy code
+#sudo ./setup-openssl.sh
+#The script will install OpenSSL, generate a self-signed certificate, set permissions for the private key, and display the generated certificate information.
 
-Please note that this script assumes a Debian-based Linux system and uses specific commands for package management and OpenSSL operations. Adjustments may be required for different Linux distributions or variations in the package management system and OpenSSL commands.
+#Please note that this script assumes a Debian-based Linux system and uses specific commands for package management and OpenSSL operations. Adjustments may be required for different Linux distributions or variations in the package management system and OpenSSL commands.
 
 
 
