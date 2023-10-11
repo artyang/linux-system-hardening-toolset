785b619dab5e63ab59649ad3a2e82477cae7c205
AlphaKiloNine <33735895+artyang@users.noreply.github.com>
2023-10-11 12:07:52 -0700

    Update install_nessus.sh

--- install_nessus.sh
+++ install_nessus.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 curl --request GET \
-  --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.6.1-ubuntu1404_i386.deb' \
-  --output 'Nessus-10.6.1-ubuntu1404_i386.deb'
+  --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.6.1-ubuntu1404_amd64.deb' \
+  --output 'Nessus-10.6.1-ubuntu1404_amd64.deb'
