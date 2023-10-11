5d34732cf88c99b40ba7138fe553209dc416d784
AlphaKiloNine <33735895+artyang@users.noreply.github.com>
2023-10-11 12:11:16 -0700

    Update install_nessus.sh

--- install_nessus.sh
+++ install_nessus.sh
@@ -3,3 +3,7 @@ curl --request GET \
   --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.6.1-ubuntu1404_amd64.deb' \
   --output 'Nessus-10.6.1-ubuntu1404_amd64.deb'
 sudo dpkg -i ./Nessus-10.6.1-ubuntu1404_amd64.deb 
+# You can start Nessus Scanner by typing /bin/systemctl start nessusd.service
+/bin/systemctl start nessusd.service
+# Then go to https://kali:8834/ to configure your scanner
+
